ed4db7e0a5d9e4a3c2fd754f7120dcc8a8abe2e9
Шеин Дмитрий Игоревич <shein-di@cbddperm.ru>
2020-05-20 16:35:06 +0500

    Initial commit

---
+++ src/main/java/ru/cbdd/fvf/FvfApplication.java
@@ -0,0 +1,13 @@
+package ru.cbdd.fvf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FvfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FvfApplication.class, args);
+    }
+
+}
