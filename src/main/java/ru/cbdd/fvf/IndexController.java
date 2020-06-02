46b475de703ec097bbe5bad20d7f8305b28eb0dc
shein <msh86.02.18@rambler.ru>
2020-06-02 17:43:13 +0500

    заполнил application.properties

---
+++ src/main/java/ru/cbdd/fvf/IndexController.java
@@ -0,0 +1,20 @@
+package ru.cbdd.fvf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    @Autowired
+    public IndexController() {
+    }
+
+    @GetMapping
+    public String mainPage() {
+        return "index";
+    }
+}
