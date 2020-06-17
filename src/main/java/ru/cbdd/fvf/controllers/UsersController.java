9ca787b16a08b540f50d0ecd6d975e1e473dd7a7
shein <msh86.02.18@rambler.ru>
2020-06-17 13:01:30 +0500

    добавил страницу с пользователями и контроллер для нее

--- src/main/java/ru/cbdd/fvf/controllers/UsersController.java
+++ src/main/java/ru/cbdd/fvf/controllers/UsersController.java
@@ -2,21 +2,19 @@ package ru.cbdd.fvf.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/index")
-public class IndexController {
+@RequestMapping("/users")
+public class UsersController {
 
     @Autowired
-    public IndexController() {
+    public UsersController() {
     }
 
     @GetMapping
-    public String mainPage() {
-        return "index";
+    public String userPage() {
+        return "users";
     }
-
 }
