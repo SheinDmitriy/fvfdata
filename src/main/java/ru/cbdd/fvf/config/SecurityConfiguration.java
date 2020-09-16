bc811515367e90fa1f8c099251907e04c3e478ab
shein <msh86.02.18@rambler.ru>
2020-09-16 17:24:55 +0500

    небольшие правки хтмлки

--- src/main/java/ru/cbdd/fvf/config/SecurityConfiguration.java
+++ src/main/java/ru/cbdd/fvf/config/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                 .permitAll()
                 .and()
                 .logout()
-                .logoutSuccessUrl("/index")
+                .logoutSuccessUrl("/")
                 .permitAll();
     }
 
