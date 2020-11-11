2a20b873f1c683918ffbb4f1296d0a72b9720edf
shein <msh86.02.18@rambler.ru>
2020-11-11 16:21:46 +0500

    добавил скрипт инициализации таблицы тип_сомплекса

---
+++ src/main/resources/db/migration/V15__initialize_complex_type.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS complex_type;
+
+create table complex_type (
+        id bigint not null auto_increment,
+        complex_type varchar(255),
+        primary key (id)
+    ) engine=InnoDB
