36bd30d9a06b8efb358632c053218df9fdace14e
shein <msh86.02.18@rambler.ru>
2020-11-11 16:54:37 +0500

    добавил скрипт инициализации таблицы комплекс_нарушения

---
+++ src/main/resources/db/migration/V19__initialize_complex_offense.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS complex_offense;
+
+CREATE TABLE complex_offense (
+  complex_id               bigint NOT NULL,
+  offense_id               bigint NOT NULL,
+
+  PRIMARY KEY (complex_id,offense_id),
+
+  CONSTRAINT FK_COMPLEX_ID FOREIGN KEY (complex_id)
+  REFERENCES complex (id)
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+  CONSTRAINT FK_DOCS_ID FOREIGN KEY (offense_id)
+  REFERENCES offense (id)
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
