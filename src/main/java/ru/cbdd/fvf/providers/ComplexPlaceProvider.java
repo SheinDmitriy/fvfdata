f34b12d819109875af48cbf090c914a646e276f1
shein <msh86.02.18@rambler.ru>
2020-11-23 15:41:19 +0500

    добавил провайдер для ComplexPlace

---
+++ src/main/java/ru/cbdd/fvf/providers/ComplexPlaceProvider.java
@@ -0,0 +1,15 @@
+package ru.cbdd.fvf.providers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.sql2o.Sql2o;
+import ru.cbdd.fvf.interfaces.iproviders.IComplexPlaceProvider;
+
+@Component
+public class ComplexPlaceProvider implements IComplexPlaceProvider {
+    private final Sql2o sql2o;
+
+    public ComplexPlaceProvider(@Autowired Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+}
