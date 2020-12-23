3e6ec05794ee338da582616650a3a598f874ca3a
shein <msh86.02.18@rambler.ru>
2020-12-23 11:51:32 +0500

    откат обратно по структуре папок

--- src/main/java/ru/cbdd/fvf/controllers/UsersController.java
+++ src/main/java/ru/cbdd/fvf/controllers/UsersController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.cbdd.fvf.entitys.ComplexDB;
 import ru.cbdd.fvf.entitys.SystemUser;
 import ru.cbdd.fvf.service.ComplexService;
 import ru.cbdd.fvf.service.RoleService;
